45ea2d545abfb6b135b52d02704e6d12d20f4b20
STARTRACEX <startraceebr@foxmail.com>
2024-02-25 03:31:34 +0800

    Change styled to tagged

--- index.ts
+++ index.ts
@@ -1,43 +1,22 @@
-import toString, { TemplateStringFunction } from "./tostring.js";
-import { createInlined, createTagged, toCSSProperties, type StylerComponent } from "./utils.js";
+import toString, { type TemplateStringFunction } from "./tostring.js";
+import {
+  createInlined,
+  createTagged,
+  toCSSProperties,
+  type StylerComponent,
+} from "./create.js";
 
-let defaultTagName = "div";
-export function SetDefaultTagName(a: string) {
-  defaultTagName = a;
-}
-
-/**
- * JSX:
- * ```jsx
- * const Red = inlined`color:red;`
- * <Red><h1>text</h1></Red>
- * ```
- * as
- * ```html
- * <h1 style="color:red;">text</h1>
- * ```
- * error:
- * ```jsx
- * <Red><h1>text</h1><del>excess</del></Red>
- * ```
- */
-export const inlined: TemplateStringFunction<StylerComponent> = (strings: TemplateStringsArray, ...values: any[]) => {
+const inlined: TemplateStringFunction<StylerComponent> = (
+  strings: TemplateStringsArray,
+  ...values: any[]
+) => {
   const string = toString(strings, ...values);
   return createInlined(string);
 };
 
-/**
- * JSX:
- * ```js
- * const Red = tagged`h1``color:red;`
- * <Red>text</Red>
- * ```
- * as
- * ```html
- * <h1 style="color:red;">text</h1>
- * ```
- */
-export const tagged: TemplateStringFunction<TemplateStringFunction<StylerComponent>> = (strings: TemplateStringsArray, ...values: any[]) => {
+const styled: TemplateStringFunction<
+  TemplateStringFunction<StylerComponent>
+> = (strings: TemplateStringsArray, ...values: any[]) => {
   const tagName = toString(strings, ...values);
   return (strings: TemplateStringsArray, ...values: any[]) => {
     const string = toString(strings, ...values);
@@ -45,32 +24,18 @@ export const tagged: TemplateStringFunction<TemplateStringFunction<StylerCompone
   };
 };
 
-/**
- * styled = {@link tagged}\`h1\` \
- * JSX:
- * ```jsx
- * const Red = styled`color:red;`
- * <Red>text</Red>
- * ```
- * as
- * ```html
- * <h1 style="color:red;">text</h1>
- * ```
- */
-export const styled: TemplateStringFunction<StylerComponent> = (strings: TemplateStringsArray, ...values: any[]) => {
-  return tagged`${defaultTagName}`(strings, ...values);
-};
-
-/**
- * <ReactElement style={stylobj`color:red;`}></ReactElement>
- * <ReactElement style={css`color:red;`}></ReactElement>
- */
-export const css: TemplateStringFunction<React.CSSProperties> = (strings: TemplateStringsArray, ...values: any[]) => {
+const css: TemplateStringFunction<React.CSSProperties> = (
+  strings: TemplateStringsArray,
+  ...values: any[]
+) => {
   const string = toString(strings, ...values);
   return toCSSProperties(string);
 };
 
-export default styled;
+const tagged = styled;
 
-export * from "./tostring.js";
-export * from "./utils.js";
+export { toString, createInlined, createTagged, toCSSProperties };
+
+export { inlined, styled, css, tagged };
+
+export default styled;
